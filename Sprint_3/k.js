2e5adfd02166d6bea102fc71bc8e0721f76372b2
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-07-24 10:27:05 +0300

    + sprint_3 K part2

--- Sprint_3/k.js
+++ Sprint_3/k.js
@@ -11,46 +11,93 @@ function merge_sort(arr, left, right) {
     merge(arr, left, mid, right)
 }
 
-function merge(arr, left, mid, right) {
-    const leftArr = [];
-    const rightArr = [];
+function merge(arr, left, mid, right)
+{
+    let n1 = mid - left;
+    let n2 = right - mid ;
 
-    for (let i = left; i <= right - 1; i++) {
-        if (i < mid) {
-            leftArr.push(arr[i]);
-        } else {
-            rightArr.push(arr[i]);
-        }
-    }
+    let L = new Array(n1);
+    let R = new Array(n2);
+
+    for (let i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (let j = 0; j < n2; j++)
+        R[j] = arr[mid + j];
 
     let i = 0;
     let j = 0;
+    let k = left;
 
-    while (i < leftArr.length && j < rightArr.length) {
-        if (leftArr[i] < rightArr[j]) {
-            arr[left] = leftArr[i];
-            i++
-        } else {
-            arr[left] = rightArr[j];
-            j++
+    // console.log(L)
+    // console.log(R)
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
         }
-        left++;
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
 
-    while (i < leftArr.length) {
-        arr[left] = leftArr[i];
+    while (i < n1) {
+        arr[k] = L[i];
         i++;
-        left++;
+        k++;
     }
 
-    while (j < rightArr.length) {
-        arr[left] = rightArr[j];
+    while (j < n2) {
+        arr[k] = R[j];
         j++;
-        left++;
+        k++;
     }
 
     return arr;
 }
+
+// function merge(arr, left, mid, right) {
+//     const leftArr = [];
+//     const rightArr = [];
+//
+//     for (let i = left; i <= right - 1; i++) {
+//         if (i < mid) {
+//             leftArr.push(arr[i]);
+//         } else {
+//             rightArr.push(arr[i]);
+//         }
+//     }
+//
+//     let i = 0;
+//     let j = 0;
+//
+//     while (i < leftArr.length && j < rightArr.length) {
+//         if (leftArr[i] < rightArr[j]) {
+//             arr[left] = leftArr[i];
+//             i++
+//         } else {
+//             arr[left] = rightArr[j];
+//             j++
+//         }
+//         left++;
+//     }
+//
+//     while (i < leftArr.length) {
+//         arr[left] = leftArr[i];
+//         i++;
+//         left++;
+//     }
+//
+//     while (j < rightArr.length) {
+//         arr[left] = rightArr[j];
+//         j++;
+//         left++;
+//     }
+//
+//     return arr;
+// }
 
 function test() {
     var a = [1, 4, 9, 2, 10, 11];
@@ -62,6 +109,10 @@ function test() {
     expected = [1, 1, 2, 2, 4, 10];
 }
 
-var a = [1, 4, 9, 2, 10, 11];
-var b = merge(a, 0, 3, 6);
-console.log(b)
+// var a = [1, 4, 9, 2, 10, 11];
+// var b = merge(a, 0, 3, 6);
+// console.log(b)
+
+// var c = [1, 4, 2, 10, 1, 2];
+// merge_sort(c, 0, 6)
+// console.log(c)
