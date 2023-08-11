de47fb2cd354ddd9bdeb53b1e452cb8e21c2d1a6
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-08-11 17:22:08 +0300

    + sprint_4 I

--- Sprint_4/i.js
+++ Sprint_4/i.js
@@ -1,51 +1,61 @@
 function genSubArr(arr1, arr2) {
-    const map = {};
     let res = 0;
+    const map = {};
 
-    for (let i = 0; i < arr1.length; i++) {
-        if (map[arr1[i]]) {
-            map[arr1[i]].push(i)
-        } else {
-            map[arr1[i]] = [i];
-        }
-    }
+    const set = new Set(arr1.filter(item => arr2.includes(item)));
 
-    for (let i = 0; i < arr2.length - 1; i++) {
-        let count = 0;
-
-        if (map[arr2[i]]) {
-            let pos = map[arr2[i]];
-            count = 1;
-
-            for (j = i + 1; j < arr2.length; j++) {
-                if (!map[arr2[j]]) break;
-
-                const next = map[arr2[j]];
-                pos = next.filter(item => pos.includes(item - 1))
-                if (pos.length) {
-                    count++;
-                    i = j;
+    for (let j of set) {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] === j) {
+                if (map[arr1[i]]) {
+                    map[arr1[i]][0].push(i)
+                } else {
+                    map[arr1[i]] = [[i],[]];
                 }
             }
         }
 
-        if (count) res = Math.max(count, res);
+        for (let i = 0; i < arr2.length; i++) {
+            if (arr2[i] === j) {
+                map[arr2[i]][1].push(i)
+            }
+        }
+
     }
 
-    if (arr2.length === 1) {
-        if (arr1.includes(arr2[0])) res = 1;
+    for (values of Object.values(map)) {
+        for (let i = 0; i < values[0].length; i++) {
+            for (j = 0; j < values[1].length; j++) {
+                let count = 1;
+
+                // console.log(values[0][i], values[0][j])
+
+                while (count + values[0][i] < arr1.length && count + values[1][j] < arr2.length) {
+                    if (arr1[count + values[0][i]] === arr2[count + values[1][j]]) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (count > res) {
+                    res = count;
+                    if (res > 100) return res;
+                }
+            }
+        }
     }
 
     return res;
 }
 
-
+// console.log(genSubArr([1,1,1,2,3,4,9,9],[1,1,1,5,6,7,9,9]));
 // console.log(genSubArr([1],[1]))
-// console.log(genSubArr([1,2,3],[3,1,2]))
+//console.log(genSubArr([1,2,3],[3,1,2]))
 // console.log(genSubArr([11, 22, 33, 22, 111], [33, 22, 11, 55, 66]))
 // console.log(genSubArr([1,1,1,1,1], [1,1,1,1,1]))
 // console.log(genSubArr([1,1,1,1,1], [1,1,2,1,1]))
-// console.log(genSubArr([1, 2, 3, 2, 1, 3], [3, 2, 1, 5, 6, 3]))
+//console.log(genSubArr([1, 2, 3, 2, 1, 3], [3, 2, 1, 5, 6, 3]))
 // console.log(genSubArr([3, 2, 1, 5, 6], [1,2,3,2,1]))
 // console.log(genSubArr([1, 2, 3, 4, 5], [4, 5, 9]))
 // console.log(genSubArr([238, 200, 244, 226, 59, 50, 165, 95, 245, 27, 73, 97, 77, 149, 21, 88, 46, 162, 72, 119, 152, 136, 134, 50, 240, 34, 57, 28, 170, 188], [251, 5, 68, 16, 226, 201, 33, 222, 122, 3, 58, 251, 250, 243, 133, 117, 74, 71, 30, 34, 179, 23, 253, 240, 30, 106, 14, 186, 50, 237]))
@@ -68,7 +78,7 @@ function readInt() {
 }
 
 function readArray() {
-    var arr = _inputLines[_curLine].trim(" ").split(" ").map(num => Number(num));
+    var arr = _inputLines[_curLine].trim(" ").split(" ");
     _curLine++;
     return arr;
 }
--- test.js
+++ test.js
@@ -1,4 +1,62 @@
-30
-156, 176, 45, 212, 116, 8, 165, 5, 15, 111, 147, 223, 30, 119, 175, 139, 131, 205, 199, 138, 178, 26, 80, 76, 180, 67, 125, 109, 83, 12
-30
-105, 200, 10, 213, 83, 123, 168, 74, 226, 43, 44, 228, 185, 188, 49, 104, 78, 200, 162, 193, 20, 7, 64, 125, 197, 199, 197, 184, 201, 59
+// const L = [1, 2, 3, 2, 1, 3];
+// const M = [3, 2, 1, 5, 6, 3];
+
+const L = [1, 1, 1, 1, 1, 1];
+const M = [1, 1, 1, 1, 1, 1];
+
+let n = 6;
+let m = 6;
+let Maxi = 0;
+let E = new Set();
+
+E = new Set(L.filter((x) => M.includes(x)));
+console.log(E);
+
+const f = (Y, a) => {
+    let k = -1;
+    const ans = [];
+    while (Y.length - k >= 0) {
+        try {
+            k = Y.indexOf(a, k + 1);
+            if (k >= 0 && k < Y.length) {
+                ans.push(k);
+            } else {
+                break;
+            }
+        } catch (error) {
+            break;
+        }
+    }
+    return ans;
+};
+
+const d = {};
+
+for (const x of E) {
+    d[x] = [f(L, x), f(M, x)];
+}
+
+console.log(d)
+
+for (const W of Object.values(d)) {
+    for (const i of W[0]) {
+        for (const j of W[1]) {
+            let k = 1;
+            console.log(i,j)
+
+            while (k + i < n && k + j < m) {
+                if (L[k + i] === M[k + j]) {
+                    k += 1;
+                } else {
+                    break;
+                }
+            }
+
+            if (k > Maxi) {
+                Maxi = k;
+            }
+        }
+    }
+}
+
+console.log(Maxi);
