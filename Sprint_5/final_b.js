334a252296fc3127054f2cc0a079333e196105cc
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-09-08 14:06:49 +0300

    + sprint_5 final_B

--- Sprint_5/final_b.js
+++ Sprint_5/final_b.js
@@ -1,4 +1,6 @@
-//if (process.env.REMOTE_JUDGE !== 'true') {
+// https://contest.yandex.ru/contest/24810/run-report/90242237/
+
+if (process.env.REMOTE_JUDGE !== 'true') {
     class Node {
         constructor(value, left = null, right = null) {
             this.value = value;
@@ -6,120 +8,127 @@
             this.right = right;
         }
     }
-//}
+}
 
 
-function treeShow(head, prefix = "head", line = "") {
+function treePrint(head, prefix = "head", line = "") {
     if (!head) {
         return;
     }
 
     console.log(prefix + line, head.value)
 
-    treeShow(head.left, "left", line + "-")
-    treeShow(head.right, "right", line + "-")
+    treePrint(head.left, "left", line + "-")
+    treePrint(head.right, "right", line + "-")
+}
+
+function searchNode(DNode, DParent, key) {
+    while (DNode && DNode.value !== key) {
+        DParent = DNode;
+
+        if (key >= DNode.value) {
+            DNode = DNode.right;
+        } else {
+            DNode = DNode.left;
+        }
+    }
+
+    return [DNode, DParent]
+}
+
+function findReplaceNode(PNode, PParent) {
+    while (PNode.right) {
+        PParent = PNode;
+        PNode = PNode.right;
+    }
+
+    return [PNode, PParent];
 }
 
 function remove(node, key) {
-    let current = node;
-    let parent = null;
+    let DNode = node;
+    let DParent = null;
 
-    while (current && current.value !== key) {
-        if (key >= current.value) {
-            parent = current;
-            current = current.right;
-        } else {
-            parent = current;
-            current = current.left;
-        }
-    }
+    [DNode, DParent] = searchNode(DNode, DParent, key);
 
-    if (!current) {
+    if (!DNode) {
         return node;
     }
 
-    let change = current;
-    let changeParent = null;
-
-    if (current.left) {
-        change = current.left;
-
-        while (change.right) {
-            changeParent = change;
-            change = change.right;
-        }
-
-        //changeParent.right = null;
-    } else {
-        if (current.right) {
-            change = current.right;
-
-            while (change.left) {
-                changeParent = change;
-                change = change.left;
+    if (!DNode.left && !DNode.right) {
+        if (DNode === node) {
+            return null;
+        } else {
+            if (DParent.left === DNode) {
+                DParent.left = null;
+            } else {
+                DParent.right = null;
             }
-
-            //changeParent.left = null;
         }
+
+        return node;
     }
 
-    ///////
+    if (DNode.left && DNode.right) {
 
-    // if (!change.left && !change.right) {
-    //     change.right = current.right;
-    //     change.left = current.left;
-    //
-    //     if (parent.right.value === key) {
-    //         parent.right = change;
-    //     } else {
-    //         parent.left = change;
-    //     }
-    // }
+        let PParent = DNode;
+        [PNode, PParent] = findReplaceNode(DNode.left, PParent);
 
-    console.log('------')
-    treeShow(current)
-    console.log('------')
-    treeShow(parent)
-    console.log('Элемент который меняем')
-    treeShow(change)
-    console.log('Предыдущий заменяемый элемент')
-    treeShow(changeParent)
-    console.log('Нода конечная')
-    treeShow(node)
+        DNode.value = PNode.value;
 
+        if (PParent.left === PNode) {
+            PParent.left = PNode.left;
+        } else {
+            PParent.right = PNode.left;
+        }
+
+    } else {
+        let PNode;
+
+        if (DNode.left) {
+            PNode = DNode.left;
+        } else {
+            PNode = DNode.right;
+        }
+
+        if (DNode !== node) {
+            if (DNode === DParent.left) {
+                DParent.left = PNode;
+            } else {
+                DParent.right = PNode;
+            }
+        } else {
+            node = PNode;
+        }
+    }
 
     return node;
 }
 
 function test() {
-    const l1 = 0
+    // var node8 = new Node(2.5, null, null);
+    // var node4 = new Node(3, node8, null);
+    // var node7 = new Node(4.4, null, null);
+    // var node6 = new Node(4.5, node7, null);
+    // var node5 = new Node(5, node6, null);
+    // var node2 = new Node(4, node4, node5);
+    // var node9 = new Node(8.5, null, null);
+    // var node3 = new Node(9, node9, null);
+    // var node1 = new Node(7, node2, node3);
+    // var main = new Node(15, node1, null);
+    // var newHead = remove(main, 8.5);
 
-    var node1 = new Node(1, null, null);
-    var node2 = new Node(3, null, null);
-    var node3 = new Node(2, node1, node2);
-    var node4 = new Node(5, null, null);
-    var node5 = new Node(7, null, null);
-    var node6 = new Node(6, node4, node5);
-    var left = new Node(4, node3, node6);
+    var node1 = new Node(2, null, null);
+    var node2 = new Node(3, node1, null);
+    var node3 = new Node(1, null, node2);
+    var node4 = new Node(6, null, null);
+    var node5 = new Node(8, node4, null);
+    var node6 = new Node(10, node5, null);
+    var node7 = new Node(5, node3, node6);
+    var newHead = remove(node7, 5);
 
-    var node7 = new Node(9, null, null);
-    var node8 = new Node(11, null, null);
-    var node9 = new Node(10, node7, node8);
-    var node10 = new Node(13, null, null);
-    var node11 = new Node(15, null, null);
-    var node12 = new Node(14, node10, node11);
-    var right = new Node(12, node9, node12);
-
-    var main = new Node(8, left, right);
-
-
-    var newHead = remove(main, 14);
-
-    //console.log(newHead)
-    //console.log(newHead)
-    //console.assert(newHead.value === 5);
-    //console.assert(newHead.right === node5);
-    // console.assert(newHead.right.value === 8);
+    console.log("\n----- Result\n")
+    treePrint(newHead)
 }
 
-test()
+//test()
--- test3.js
+++ test3.js
@@ -1,100 +1,124 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function main() {
-    let n;
-    const text_dict = {};
-
-    rl.question('Enter the value of n: ', (nInput) => {
-        n = parseInt(nInput);
-
-        let count = 0;
-        function processLine() {
-            rl.question('Enter a line of text: ', (line) => {
-                const words = line.trim().split(' ');
-
-                const row_count = count + 1;
-                for (const word of words) {
-                    if (word in text_dict) {
-                        if (row_count in text_dict[word]) {
-                            text_dict[word][row_count] += 1;
-                        } else {
-                            text_dict[word][row_count] = 1;
-                        }
-                    } else {
-                        text_dict[word] = { [row_count]: 1 };
-                    }
-                }
-
-                count++;
-                if (count < n) {
-                    processLine();
-                } else {
-                    processSearch();
-                }
-            });
-        }
-
-        function processSearch() {
-            let m;
-            rl.question('Enter the value of m: ', (mInput) => {
-                m = parseInt(mInput);
-
-                let search_count = 0;
-                function processSearchLine() {
-                    rl.question('Enter a line of search words: ', (line) => {
-                        const search_words = line.trim().split(' ');
-
-                        const unique_words = new Set();
-                        const search = {};
-
-                        for (const word of search_words) {
-                            if (word in text_dict && !unique_words.has(word)) {
-                                unique_words.add(word);
-                                for (const doc_dict in text_dict[word]) {
-                                    const relevance = text_dict[word][doc_dict];
-                                    if (doc_dict in search) {
-                                        search[doc_dict] += relevance;
-                                    } else {
-                                        search[doc_dict] = relevance;
-                                    }
-                                }
-                            }
-                        }
-
-                        console.log(text_dict);
-
-                        let count = 0;
-                        for (const [key, value] of Object.entries(search).sort((a, b) => b[1] - a[1] || a[0] - b[0])) {
-                            if (count === 5) {
-                                break;
-                            }
-                            process.stdout.write(`${key} `);
-                            count++;
-                        }
-                        if (Object.keys(search).length > 0) {
-                            process.stdout.write('\n');
-                        }
-
-                        search_count++;
-                        if (search_count < m) {
-                            processSearchLine();
-                        } else {
-                            rl.close();
-                        }
-                    });
-                }
-
-                processSearchLine();
-            });
-        }
-
-        processLine();
-    });
+// Helper function to find the node with the minimum key in the subtree with root `curr`
+function getMinimumKey(curr) {
+    while (curr.left !== null) {
+        curr = curr.left;
+    }
+    return curr;
 }
 
-main();
+// Recursive function to insert a key into the BST
+function insert(root, key) {
+    // if the root is null, create a new node and return it
+    if (root === null) {
+        return { data: key, left: null, right: null };
+    }
+
+    // if the given key is less than the root node, recurse for the left subtree
+    if (key < root.data) {
+        root.left = insert(root.left, key);
+    }
+
+    // if the given key is greater than the root node, recurse for the right subtree
+    else {
+        root.right = insert(root.right, key);
+    }
+
+    return root;
+}
+
+// Iterative function to search for a key in the subtree with root `curr` and set its parent.
+// Note that `curr` and `parent` are passed by reference to the function.
+function searchKey(curr, key, parent) {
+    // traverse the tree and search for the key
+    while (curr !== null && curr.data !== key) {
+        // update the parent to the current node
+        parent[0] = curr;
+
+        // if the given key is less than the current node, go to the left subtree;
+        // otherwise, go to the right subtree
+        if (key < curr.data) {
+            curr = curr.left;
+        }
+        else {
+            curr = curr.right;
+        }
+    }
+}
+
+// Function to delete a node from the BST
+function deleteNode(root, key) {
+    // pointer to store the parent of the current node
+    let parent = [null];
+
+    // start with the root node
+    let curr = root;
+
+    // search for the key in the BST and set its parent pointer
+    searchKey(curr, key, parent);
+
+    // return if the key is not found in the tree
+    if (curr === null) {
+        return;
+    }
+
+    // Case 1: the node to be deleted is a leaf node (i.e., it has no children)
+    if (curr.left === null && curr.right === null) {
+        // if the node to be deleted is not the root node, set its parent's left/right child to null
+        if (curr !== root) {
+            if (parent[0].left === curr) {
+                parent[0].left = null;
+            }
+            else {
+                parent[0].right = null;
+            }
+        }
+        // if the tree has only the root node, set it to null
+        else {
+            root = null;
+        }
+
+        // free the memory
+        curr = null;
+    }
+
+    // Case 2: the node to be deleted has two children
+    else if (curr.left && curr.right) {
+        // find its in-order successor node
+        let successor = getMinimumKey(curr.right);
+
+        // save the value of the successor node
+        let val = successor.data;
+
+        // recursively delete the successor node. Note that the successor
+        // will have at most one child (right child)
+        deleteNode(root, successor.data);
+
+        // copy the value of the successor node to the current node
+        curr.data = val;
+    }
+
+    // Case 3: the node to be deleted has only one child
+    else {
+        // select the child node
+        let child = (curr.left !== null) ? curr.left : curr.right;
+
+        // if the node to be deleted is not the root node, set its parent
+        // to its child node
+        if (curr !== root) {
+            if (curr === parent[0].left) {
+                parent[0].left = child;
+            }
+            else {
+                parent[0].right = child;
+            }
+        }
+
+        // if the node to be deleted is the root node, set the root to its child node
+        else {
+            root = child;
+        }
+
+        // free the memory
+        curr = null;
+    }
+}
