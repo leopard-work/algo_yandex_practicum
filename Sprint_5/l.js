bc4e1da8d22fcef4e6ec4a779c60c21d2e349f09
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-09-12 14:57:10 +0300

    + sprint_5 L fix

--- Sprint_5/l.js
+++ Sprint_5/l.js
@@ -1,15 +1,9 @@
 function siftDown(heap, idx) {
-    let leftIndex = idx * 2
-    let rightIndex = idx * 2 + 1;
-    let swapIndex = leftIndex;
+    let leftIndex;
+    let rightIndex;
+    let swapIndex;
 
-    if (rightIndex < heap.length && heap[leftIndex] < heap[rightIndex]) {
-        swapIndex = rightIndex;
-    }
-
-    while (leftIndex < heap.length && heap[idx] < heap[swapIndex]) {
-        [heap[idx], heap[swapIndex]] = [heap[swapIndex], heap[idx]];
-        idx = swapIndex;
+    const check = () => {
         leftIndex = idx * 2
         rightIndex = idx * 2 + 1;
         swapIndex = leftIndex;
@@ -17,14 +11,26 @@ function siftDown(heap, idx) {
         if (rightIndex < heap.length && heap[leftIndex] < heap[rightIndex]) {
             swapIndex = rightIndex;
         }
+
+        if (rightIndex < heap.length && heap[leftIndex] < heap[rightIndex]) {
+            swapIndex = rightIndex;
+        }
+    }
+
+    check();
+
+    while (leftIndex < heap.length && heap[idx] < heap[swapIndex]) {
+        [heap[idx], heap[swapIndex]] = [heap[swapIndex], heap[idx]];
+        idx = swapIndex;
+        check();
     }
 
     return idx;
 }
 
-function test() {
-    var sample = [-1, 12, 1, 8, 3, 4, 7];
-    console.assert(siftDown(sample, 2) === 5);
-}
-
-//test();
+// function test() {
+//     var sample = [-1, 12, 1, 8, 3, 4, 7];
+//     console.assert(siftDown(sample, 2) === 5);
+// }
+//
+// test();
