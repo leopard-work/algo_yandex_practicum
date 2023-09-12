27c149384aa2c793282519cd8994e031bb47395d
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-09-12 10:34:05 +0300

    + sprint_5 B

--- Sprint_5/b.js
+++ Sprint_5/b.js
@@ -1,4 +1,4 @@
-//if (process.env.REMOTE_JUDGE !== 'true') {
+if (process.env.REMOTE_JUDGE !== 'true') {
     class CNode {
         constructor(value) {
             this.value = value;
@@ -6,36 +6,40 @@
             this.right = null;
         }
     }
-//}
-
-function solution(root) {
-
-    console.log(isBalace(root.left, root.right, 0))
-
-    return true;
 }
 
-function isBalace(left, right, count) {
-    if (Math.abs(count) > 1) {
-        return false;
+function solution(root) {
+    return dfs(root) !== -1;
+}
+
+function dfs(node) {
+    if (!node) {
+        return 0;
     }
 
-    if (!left) {
-        count--;
-    } else {
-        isBalace(left.left, left.right, count);
+    const left = dfs(node.left);
+
+    if (left === -1) {
+        return -1;
     }
-    if (!right) {
-        count++;
-    } else {
-        isBalace(right?.left, right?.right, count);
+
+    const right = dfs(node.right);
+
+    if (right === -1) {
+        return -1;
     }
+
+    if (Math.abs(left - right) > 1) {
+        return -1;
+    }
+
+    return Math.max(left, right) + 1;
 }
 
 function test() {
     var node0 = new CNode(0);
     var node1 = new CNode(1);
-    node1.left = node0;
+    //node1.left = node0;
     var node2 = new CNode(-5);
     var node3 = new CNode(3);
     node3.left = node1;
@@ -44,7 +48,8 @@ function test() {
     var node5 = new CNode(2);
     node5.left = node3;
     node5.right = node4;
+    console.log(node5)
     console.assert(solution(node5));
 }
 
-test();
+//test();
