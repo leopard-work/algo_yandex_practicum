ef0f4942becf56a2ce3dec8005df116078e97186
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-09-15 14:35:59 +0300

    + sprint_5 N tmp

--- Sprint_5/n.js
+++ Sprint_5/n.js
@@ -1,3 +1,74 @@
+//if (process.env.REMOTE_JUDGE !== 'true') {
+    class Node {
+        constructor(value, left = null, right = null, size = 0) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+            this.size = size;
+        }
+    }
+//}
+
+function treePrint(head, prefix = "head", line = "") {
+    if (!head) {
+        return;
+    }
+
+    console.log(prefix + line, head.value)
+
+    treePrint(head.left, "left", line + "-")
+    treePrint(head.right, "right", line + "-")
+}
+
+function getSize(node) {
+    if (node) {
+        return node.size;
+    }
+
+    return 0;
+}
+
+function split(node, k) {
+    if (node === null) {
+        return [null, null];
+    }
+
+    const leftSize = getSize(node.left);
+    const rightSize = getSize(node.right);
+
+    if (leftSize + 1 <= k) {
+        k -= leftSize + 1;
+        const [right, rightSide] = split(node.right, k);
+        node.right = right;
+        return [rightSide, node];
+    }
+
+    const [left, leftSide] = split(node.left, k);
+    node.left = left;
+    return [leftSide, node];
+}
+
+function test() {
+    const node1 = new Node(3, null, null, 1);
+    const node2 = new Node(2, null, node1, 2);
+    const node3 = new Node(8, null, null, 1);
+    const node4 = new Node(11, null, null, 1);
+    const node5 = new Node(10, node3, node4, 3);
+    const node6 = new Node(5, node2, node5, 6);
+    const res = split(node6, 1);
+
+    treePrint(res[0]);
+    console.log('---')
+    treePrint(res[1]);
+
+    //console.assert(res[0].size === 4);
+    //console.assert(res[1].size === 2);
+}
+
+test();
+
+
+/*
 class Node {
     constructor(value, left = null, right = null, size = 0) {
         this.value = value;
@@ -73,3 +144,4 @@ function test() {
 }
 
 test()
+ */
