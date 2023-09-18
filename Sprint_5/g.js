f0e33c7b6069aca0a5d244d3131ed23510d13f5e
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-09-18 18:19:35 +0300

    + sprint_5 G part 2

--- Sprint_5/g.js
+++ Sprint_5/g.js
@@ -10,21 +10,43 @@
 
 
 function solution(root) {
-    if (!root) {
-        return -1;
+    let [, maxTreeSum] = searchMaxPath(root);
+    //const [, result] = searchMax(root);
+    console.log(maxTreeSum)
+    return maxTreeSum;
+}
+
+function searchMax(root, result) {
+    if (root === null) {
+        return [0, result];
     }
 
-    const left = solution(root.left);
-    console.log('left', left);
-    const right = solution(root.right);
-    console.log('right', right);
+    // let [leftMax, ]
+}
 
-    return root.value;
+function searchMaxPath(root, maxSum = null) {
+    if (root === null) {
+        return [0, maxSum];
+    }
+
+    let [leftMaxSum, updatedMaxSum] = searchMaxPath(root.left, maxSum);
+    let [rightMaxSum, updatedMaxSum2] = searchMaxPath(root.right, updatedMaxSum);
+
+    let branchMaxSum = Math.max(leftMaxSum, rightMaxSum) + root.value;
+    let subtreeMaxSum = Math.max(branchMaxSum, leftMaxSum + rightMaxSum + root.value);
+
+    if (updatedMaxSum === null) {
+        updatedMaxSum = subtreeMaxSum;
+    } else {
+        updatedMaxSum = Math.max(subtreeMaxSum, updatedMaxSum);
+    }
+
+    return [branchMaxSum, updatedMaxSum];
 }
 
 function test() {
     var node1 = new CNode(5, null, null);
-    var node2 = new CNode(1, null, null);
+    var node2 = new CNode(0, null, null);
     var node3 = new CNode(-3, node2, node1);
     var node4 = new CNode(2, null, null);
     var node5 = new CNode(2, node4, node3);
