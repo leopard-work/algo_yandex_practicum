b98cc6d8b20eabdae21456cfc694fa794251e7cf
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-09-28 18:50:33 +0300

    + sprint_6 С part 1

---
+++ Sprint_6/c.js
@@ -0,0 +1,13 @@
+function dfs() {
+
+}
+
+function dfsFn(n, m, list) {
+    const colors = new Array(n).fill("white");
+
+
+
+    return colors;
+}
+
+console.log(dfsFn(4, 4, [[3,2], [4,3], [1,4], [1,2]]))
